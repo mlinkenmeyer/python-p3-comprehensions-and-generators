e9d3d03064458533949811b89ce88e6bc925c181
Madison <mlinkenmeyer@gmail.com>
2023-09-11 19:55:20 -0700

    comprehensions and generators lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_num_list = [x for x in num_list if x % 2 == 0]
+    return new_num_list
+
 
 def make_exclamation(sentence_list):
-    pass
+    new_exlamation_list = [sentence + "!" for sentence in sentence_list]
+    return new_exlamation_list
